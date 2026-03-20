1371551649b95bfb05365bf35f32931452eb2937
Sharath K R <krsharath37@gmail.com>
2026-03-20 13:18:45 +0530

    Add files via upload

---
+++ src/routers/user.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { getAllUser, getUserByID, signin, signup } from "../controllers/user";
+import { checkAdmin, requireSignin } from "../common/user";
+
+const router = express.Router();
+
+router.post("/signin", signin);
+router.post("/signup", signup);
+
+router.get("/user", requireSignin, checkAdmin, getAllUser);
+router.get("/user:_id", requireSignin, getUserByID);
+export default router;
